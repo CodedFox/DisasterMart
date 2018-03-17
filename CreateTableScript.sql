014090ea8d8d2bd361f38bdce91ee6a34966196a
mathismarcotte <mathismarcotte@hotmail.com>
2018-03-17 09:46:53 -0400

    Created Script to create the tables in the db
    If we need to edit stuff later on, we can go nuts. I wasn't sure what we were going to do with the population and the weather stuff, so I just put it in comments for now.

---
+++ CreateTableScript.sql
@@ -0,0 +1,79 @@
+/*
+drop table date;
+drop table disaster;
+drop table summary;
+drop table location;
+drop table fact;
+drop table costs;
+*/
+CREATE TABLE DATE (
+	DateKey int primary key,
+	Day int,
+	Month int,
+	Year int,
+	Weekend boolean,
+	SeasonCanada varchar(20),
+	SeasonInternational varchar(20)
+);
+
+create table DISASTER (
+	DisasterKey int primary key,
+	DisasterType varchar(50),
+	DisasterSubgroup varchar(50),
+	DisasterGroup varchar(50),
+	DisasterCategory varchar(50),
+	Magnitude int,
+	UtilityPeopleAffected int
+);
+
+Create table SUMMARY(
+	DescriptionKey int primary key,
+	Summary varchar(300),
+	Keyword1 varchar(100),
+	Keyword2 varchar(100),
+	Keyword3 varchar(100)
+);
+
+Create table LOCATION(
+	LocationKey int primary key,
+	City varchar(100),
+	Province varchar(100),
+	Country varchar(100),
+	Canada boolean
+);
+
+Create table COSTS(
+	CostsKey int primary key,
+	EstimatedTotalCost int,
+	NormalizedTotalCost numeric,
+	FederalDFAAPayments int,
+	ProvincialDFAAPayments int,
+	ProvincialDepartmentPayments int,
+	MunicipalCosts int,
+	OGDCosts int,
+	InsurancePayments int,
+	NGOPayments int
+);
+
+Create table FACT(
+	StartDateKey int,
+	EndDateKey int,
+	LocationKey int,
+	DisasterKey int,
+	DescriptionKey int,
+	CostKey int,
+	--PopStatsKey int,
+	--WeatherKey int,
+	Fatalities int,
+	Injured int,
+	Evacuated int,
+	PRIMARY KEY(StartDateKey,EndDateKey,LocationKey,DisasterKey,DescriptionKey,CostKey)
+);
+
+/*Create table WeatherInfo(
+	WeatherKey int primary key
+);
+
+Create table PopulationStats(
+	PopStatsKey int primary key
+);*/
